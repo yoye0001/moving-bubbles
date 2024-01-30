feccf2d3c7c55eb9ae2aaa29a4fe58ff0c8e45eb
Jonas Holbech <jh@jonasholbech.dk>
2024-01-30 13:31:10 +0100

    rabndom pos & color

---
+++ main.js
@@ -0,0 +1,7 @@
+const all = document.querySelectorAll(".container div");
+all.forEach((bubble) => {
+  bubble.style.transform = `translate(${Math.random() * 100}vw, ${
+    Math.random() * 100
+  }vh)`;
+  bubble.style.background = `hsl(${Math.random() * 360}, 70%, 70%)`;
+});
